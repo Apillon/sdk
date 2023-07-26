84cbc068c7e52320416c25d47494d3a34f975800
Tadej <tadej.dev@gmail.com>
2023-07-26 11:59:38 +0200

    refactor(types): move enum to types and clean up tsconfig

--- packages/sdk/src/constants/nfts.ts
+++
@@ -1,6 +0,0 @@
-export enum EvmChain {
-  MOONBEAM = 1284,
-  MOONBASE = 1287,
-  ASTAR_SHIBUYA = 81,
-  ASTAR = 592,
-}
--- packages/sdk/src/types/nfts.ts
+++ packages/sdk/src/types/nfts.ts
@@ -1,4 +1,9 @@
-import { EvmChain } from '../constants/nfts';
+export enum EvmChain {
+  MOONBEAM = 1284,
+  MOONBASE = 1287,
+  ASTAR_SHIBUYA = 81,
+  ASTAR = 592,
+}
 
 export interface ICreateCollection {
   chain: EvmChain;
