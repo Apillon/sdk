0d8b3fe6d954a02d0cdeda6b6d60e78e721ec114
Damjan Dimitrov <damjandimitrov1@gmail.com>
2023-11-29 18:03:44 +0100

    Add SDK default configuration

--- packages/sdk/src/lib/apillon.ts
+++ packages/sdk/src/lib/apillon.ts
@@ -32,9 +32,19 @@ export interface ApillonConfig {
 
 export class ApillonModule {
   public constructor(config?: ApillonConfig) {
-    ApillonApi.initialize(config);
+    const defaultConfig: ApillonConfig = {
+      key: process.env.APILLON_API_KEY,
+      secret: process.env.APILLON_API_SECRET,
+      apiUrl: process.env.APILLON_API_URL || 'https://api.apillon.io',
+      logLevel: LogLevel.ERROR,
+      debug: false,
+    };
+
+    const mergedConfig = { ...defaultConfig, ...config };
+
+    ApillonApi.initialize(mergedConfig);
     ApillonLogger.initialize(
-      config.debug ? LogLevel.VERBOSE : config.logLevel || LogLevel.ERROR,
+      mergedConfig.debug ? LogLevel.VERBOSE : mergedConfig.logLevel,
     );
   }
 }
