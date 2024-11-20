711160d394a5fd4da459c91fa19bdef918bc51e5
Damjan <damjandimitrov1@gmail.com>
2024-11-20 14:58:32 +0100

    Update SDK exports

--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -4,8 +4,13 @@ export * from './types/hosting';
 export * from './types/storage';
 export * from './types/identity';
 export * from './types/social';
+export * from './types/cloud-functions';
+export * from './types/indexer';
+export * from './types/rpc';
+
 export * from './lib/apillon';
 export * from './lib/common';
+
 export * from './modules/storage/storage';
 export * from './modules/hosting/hosting';
 export * from './modules/nft/nft';
@@ -15,4 +20,4 @@ export * from './modules/social/social';
 export * from './modules/project/project';
 export * from './modules/cloud-functions/cloud-functions';
 export * from './modules/indexing/indexing';
-export * from './modules/indexing/indexer';
+export * from './modules/rpc/rpc';
