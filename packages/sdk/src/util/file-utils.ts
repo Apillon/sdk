c7e279fde4c483296aedd1e173ea51560ed743b8
Jaka Purg <purg.jaka@gmail.com>
2025-01-08 14:10:36 +0100

    Fix for uploading multiple files with same name

--- packages/sdk/src/util/file-utils.ts
+++ packages/sdk/src/util/file-utils.ts
@@ -111,7 +111,7 @@ export async function uploadFiles(uploadParams: {
 
       // Upload doesn't return files in the same order as sent
       const sortedFiles = metadata.files.map((metaFile) =>
-        files.find((file) => file.fileName === metaFile.fileName),
+        files.find((file) => file.fileName === metaFile.fileName && file.path === metaFile.path),
       );
 
       await uploadFilesToS3(sortedFiles, fileGroup);
